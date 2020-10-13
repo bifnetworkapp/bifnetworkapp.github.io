c084963d2c45e07afcb2b56891c40b715e96c2a2
Leon Braginsky <leonbrag215@gmail.com>
2020-10-13 21:30:57 +0300

    adding missing js files

---
+++ precache-manifest.506f6c9e6518d4833137c8d138910588.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cef7d437249686d37b461db4188ea9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba6c5e0ef2d568cbf5c5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "18ba17b7ecdfe7da58f9",
+    "url": "/static/js/2.cca18a22.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.cca18a22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba6c5e0ef2d568cbf5c5",
+    "url": "/static/js/main.f0c71a52.chunk.js"
+  },
+  {
+    "revision": "c1db16a7a4c8743117ee",
+    "url": "/static/js/runtime-main.0427cceb.js"
+  }
+]);
